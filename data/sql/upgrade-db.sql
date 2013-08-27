6b0358c6797ed8e929ca7bd9d21165ff07fb1d60
joris <joris@f972ef24-6785-6244-9f4f-f595f474ce1c>
2013-08-27 07:27:18 +0000

    [TASK] updated upgrade-db.sql

--- data/sql/upgrade-db.sql
+++ data/sql/upgrade-db.sql
@@ -18,3 +18,26 @@ ALTER TABLE `brief_layout` CHANGE `id` `id` INT( 11 ) NOT NULL AUTO_INCREMENT;
 -- -----------------------------------------------------------------------------
 ALTER TABLE brief_verzonden ADD object_class_bestemmeling VARCHAR(45) NULL DEFAULT NULL AFTER object_id;
 ALTER TABLE brief_verzonden ADD object_id_bestemmeling INT NULL DEFAULT NULL AFTER object_class_bestemmeling;
+
+-- -----------------------------------------------------------------------------
+-- 27/08/2013
+-- Glenn Van Loock
+--
+-- bestemmeling toevoegen aan brief_verzonden
+-- -----------------------------------------------------------------------------
+CREATE TABLE `aanwezigheid`
+(
+	`inschrijving_id` INTEGER  NOT NULL,
+	`activiteit_periode_id` INTEGER  NOT NULL,
+	`aanwezig` INTEGER default 0,
+	`betaald` INTEGER default 0,
+	PRIMARY KEY (`inschrijving_id`,`activiteit_periode_id`),
+	KEY `aanwezigheid_I_1`(`inschrijving_id`),
+	KEY `aanwezigheid_I_2`(`activiteit_periode_id`),
+	CONSTRAINT `fk_aanwezig_inschrijving`
+		FOREIGN KEY (`inschrijving_id`)
+		REFERENCES `inschrijving` (`id`),
+	CONSTRAINT `fk_aanwezig_actPer`
+		FOREIGN KEY (`activiteit_periode_id`)
+		REFERENCES `activiteit_periode` (`id`)
+)Engine=InnoDB;
