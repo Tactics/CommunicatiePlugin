d5d70ddcc776c000f4d4c59365c2c2943c5212d7
TacoOr <Taco.orens@tacticS.be>
2015-12-17 11:04:21 +0100

    [BUGFIX] Close div manual - probleem in firefox (witte paginas)

--- modules/ttCommunicatie/templates/printSuccess.php
+++ modules/ttCommunicatie/templates/printSuccess.php
@@ -21,7 +21,7 @@
     {
       if ($aantal_brieven > 0)
       {
-        echo "\n\n<div STYLE=\"page-break-before: always\"/>\n\n";
+        echo "\n\n<div style=\"page-break-before: always\"></div>\n\n";
       }
 
       if (isset($rs))
