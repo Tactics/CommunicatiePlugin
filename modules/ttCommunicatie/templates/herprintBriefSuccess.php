303161f82a210bb412a45a9a705dc13a9b979989
GlennVl <glennvl_91@hotmail.com>
2018-03-02 10:34:58 +0100

    [PVI-484] echo van html is genoeg

--- modules/ttCommunicatie/templates/herprintBriefSuccess.php
+++ modules/ttCommunicatie/templates/herprintBriefSuccess.php
@@ -10,89 +10,13 @@
 
 <body>
   <?php
-      $objectBestemmelingen = array();
-      /** @var BriefVerzonden $briefVerzonden */
-      $object = $briefVerzonden->getObject();
-      
-      $objectBestemmelingen = array($briefVerzonden->getBestemmeling()->getTtCommunicatieBestemmeling());
-      
-      foreach ($objectBestemmelingen as $index => $bestemmeling)
-      {
-        $bestemmeling->setObject($object);
-        $email = $bestemmeling->getEmailTo();
-        
-        /** @var BriefTemplate $object_brief_template */
-        $object_brief_template = $briefVerzonden->getBriefTemplate();
-        /** @var BriefLayout $brief_layout */
-        $brief_layout = $object_brief_template->getBriefLayout();
-        
-        // onderwerp en tekst ophalen
-        $onderwerpen = $object_brief_template->getOnderwerpCultureArr();
-        $htmls = $object_brief_template->getHtmlCultureArr();
-        
-        // Culture voor object ophalen
-        $culture = BriefTemplatePeer::calculateCulture($bestemmeling);
-
-        $cultureBrieven = array();
-        foreach (BriefTemplatePeer::getCultureLabelArray() as $culture => $label)
-        {
-          $cultureBrieven[$culture] = $brief_layout->getHeadAndBody('brief', $culture, $htmls[$culture]);
-          $cultureBrieven[$culture]['onderwerp'] = $onderwerpen[$culture];
-        }
-        
-        // work with copy of culturebrieven
-        $tmpCultureBrieven = $cultureBrieven;
-
-        // parse If and foreach statements
-        $tmpCultureBrieven[$culture]['body'] = BriefTemplatePeer::parseForeachStatements($tmpCultureBrieven[$culture]['body'], $bestemmeling);
-        $tmpCultureBrieven[$culture]['body'] = BriefTemplatePeer::parseIfStatements($tmpCultureBrieven[$culture]['body'], $bestemmeling);
-
-        // replace placeholders
-        $defaultPlaceholders = BriefTemplatePeer::getDefaultPlaceholders($bestemmeling, false, false, true);
-        if (!$object_brief_template->getIsPubliciteit() && isset($defaultPlaceholders['uitschrijven']))
-        {
-          unset($defaultPlaceholders['uitschrijven']);
-        }
-        $tmpCultureBrieven = BriefTemplatePeer::replacePlaceholdersFromCultureBrieven($tmpCultureBrieven, $bestemmeling, $defaultPlaceholders);
-        $head = $tmpCultureBrieven[$culture]['head'];
-        $onderwerp = $tmpCultureBrieven[$culture]['onderwerp'];
-        $body = $tmpCultureBrieven[$culture]['body'];
-        $brief = $head . $body;
-        
-        echo BriefTemplatePeer::clearPlaceholders($brief);
-        
-        // Log de brief tijdelijk om later te kunnen bevestigen
-        if (! $voorbeeld)
-        {
-          $newBriefVerzonden = new BriefVerzonden();
-          $newBriefVerzonden->setObjectClass(get_class($object));
-          $newBriefVerzonden->setObjectId($object->getId());
-          $newBriefVerzonden->setObjectClassBestemmeling($bestemmeling->getObjectClass());
-          $newBriefVerzonden->setObjectIdBestemmeling($bestemmeling->getObjectId());
-          $newBriefVerzonden->setBriefTemplate($object_brief_template);
-          $newBriefVerzonden->setMedium(BriefverzondenPeer::MEDIUM_PRINT);
-          $newBriefVerzonden->setAdres($bestemmeling->getAdres());
-          $newBriefVerzonden->setOnderwerp($onderwerp);
-          $newBriefVerzonden->setCulture($culture);
-          $newBriefVerzonden->setHtml($brief);
-          $newBriefVerzonden->setStatus(BriefVerzondenPeer::STATUS_NT_VERZONDEN);
-          $newBriefVerzonden->save();
-
-          $brief_verzonden_ids[] = $newBriefVerzonden->getId();
-        }
-
-        $aantal_brieven++;
-      }
+      echo $briefVerzonden->getHtml();
   ?>
 
   <div class="printbox">
     <?php echo input_hidden_tag('brief_verzonden_ids', implode(',', $brief_verzonden_ids)); ?>
     <div style='float:right;text-align:right'>
       <a href="javascript:window.close()"><?php echo image_tag("icons/close.png")?></a>
-      <br/>
-      <br/>
-      <br/>
-      <span style="font-size: 9px;">Totaal aantal <?php echo $viaemail ? 'e-mails: ' . $aantal_via_email : 'brieven: ' . $aantal_brieven; ?></span>
     </div>
 
     <center>
